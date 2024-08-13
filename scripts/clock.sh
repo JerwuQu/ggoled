f19547c32d2f19a022bdadd28531a8b08cf37f8e
Marcus Ramse <marcus@ramse.se>
2024-08-13 23:29:39 +0000

    basic text command (TODOs remaining)

--- scripts/clock.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# scripts/clock.sh
-while true; do
-	"$(dirname "$0")/textpng.sh" "$(date +"%Y-%m-%d %H:%M:%S")"
-	sleep 1
-done
--- scripts/textpng.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# scripts/textpng.sh "Hello, World!"
-magick convert -size 128x64 canvas:black -pointsize 16 -fill white -font "$(dirname "$0")/PixelOperator.ttf" -draw "text 0,12 '$1'" png:- | sanpwo img -
-
