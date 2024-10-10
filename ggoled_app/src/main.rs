76a1df8763318b3a0b8e9579810be4688954f672
Marcus Ramse <marcus@ramse.se>
2024-10-10 16:21:34 +0000

    app: screensaver

--- ggoled_app/src/main.rs
+++ ggoled_app/src/main.rs
@@ -4,15 +4,28 @@ use chrono::{Local, TimeDelta, Timelike};
 use ggoled_draw::{DrawDevice, LayerId};
 use ggoled_lib::Device;
 use media::{Media, MediaControl};
-use std::{ptr::null_mut, thread::sleep, time::Duration};
+use std::{mem::size_of, ptr::null_mut, thread::sleep, time::Duration};
 use tray_icon::{
     menu::{Menu, MenuEvent, MenuItem},
     Icon, TrayIconBuilder,
 };
-use windows_sys::Win32::UI::WindowsAndMessaging::{DispatchMessageW, PeekMessageW, TranslateMessage, MSG};
+use windows_sys::Win32::{
+    System::{Console::AllocConsole, SystemInformation::GetTickCount},
+    UI::{
+        Input::KeyboardAndMouse::{GetLastInputInfo, LASTINPUTINFO},
+        WindowsAndMessaging::{DispatchMessageW, PeekMessageW, TranslateMessage, MSG},
+    },
+};
 mod media;
 
+const IDLE_TIMEOUT_SECS: usize = 60;
+
 fn main() {
+    #[cfg(debug_assertions)]
+    {
+        unsafe { AllocConsole() };
+    }
+
     let icon_png = include_bytes!("../ggoled.png");
     let icon_rgba = image::load_from_memory(icon_png)
         .unwrap()
@@ -30,8 +43,6 @@ fn main() {
         .build()
         .unwrap();
 
-    println!("Running");
-
     let mut dev = DrawDevice::new(Device::connect().unwrap(), 30);
     dev.play();
 
@@ -42,7 +53,6 @@ fn main() {
     let mut last_media: Option<Media> = None;
     let mut time_layers: Vec<LayerId> = vec![];
     let mut media_layers: Vec<LayerId> = vec![];
-    // TODO: screensaver when computer is unused or locked
     loop {
         // Window event loop is required to get tray-icon working
         unsafe {
@@ -53,41 +63,58 @@ fn main() {
             }
         }
 
-        // Update time every second
+        // Update layers every second
         let time = Local::now();
         if time.second() != last_time.second() {
-            let time_str = time.format("%H:%M:%S").to_string();
-            let media = mgr.get_media(); // also only fetch media once a second
-
-            dev.pause();
-            dev.remove_layers(&time_layers);
-            time_layers = dev.add_text(&time_str, None, Some(8));
             last_time = time;
 
-            if media != last_media {
-                dev.remove_layers(&media_layers);
-                if let Some(m) = &media {
-                    media_layers = dev.add_text(
-                        &format!("{}\n{}", m.title, m.artist),
-                        None,
-                        Some(8 + dev.font_line_height() as isize),
-                    );
+            // Check if idle
+            let idle_seconds = unsafe {
+                let mut lastinput = LASTINPUTINFO {
+                    cbSize: size_of::<LASTINPUTINFO>() as u32,
+                    dwTime: 0,
+                };
+                if GetLastInputInfo(&mut lastinput) != 0 {
+                    ((GetTickCount() - lastinput.dwTime) / 1000) as usize
                 } else {
-                    media_layers = vec![]
+                    0
                 }
-                last_media = media;
+            };
+            if idle_seconds >= IDLE_TIMEOUT_SECS {
+                dev.clear_layers(); // clear screen when idle
+            } else {
+                let time_str = time.format("%H:%M:%S").to_string();
+                let media = mgr.get_media(); // also only fetch media once a second
+
+                dev.pause();
+                dev.remove_layers(&time_layers);
+                time_layers = dev.add_text(&time_str, None, Some(8));
+
+                if media != last_media {
+                    dev.remove_layers(&media_layers);
+                    if let Some(m) = &media {
+                        media_layers = dev.add_text(
+                            &format!("{}\n{}", m.title, m.artist),
+                            None,
+                            Some(8 + dev.font_line_height() as isize),
+                        );
+                    }
+                    last_media = media;
+                }
+                dev.play();
             }
-            dev.play();
         }
 
         if let Ok(event) = menu_channel.try_recv() {
             if event.id == quit.id() {
-                dev.clear_layers();
-                dev.await_frame();
-                return;
+                break; // break main loop
             }
         }
 
         sleep(Duration::from_millis(10));
     }
+
+    // Draw a blank frame when quitting
+    dev.clear_layers();
+    dev.await_frame();
 }
