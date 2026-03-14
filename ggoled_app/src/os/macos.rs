b7184871fcf95bf4d7d6423ab268e419da626305
LagTheSystem <lagthesystem@duck.com>
2026-03-14 10:11:31 -0500

    Remove unneeded Optional wrapper from MediaControl

--- ggoled_app/src/os/macos.rs
+++ ggoled_app/src/os/macos.rs
@@ -2,16 +2,15 @@ use super::Media;
 use media_remote::NowPlayingPerl;
 
 pub struct MediaControl {
-    npp: Option<NowPlayingPerl>,
+    npp: NowPlayingPerl,
 }
 impl MediaControl {
     pub fn new() -> MediaControl {
-        let npp: Option<NowPlayingPerl> = Some(NowPlayingPerl::new());
+        let npp = NowPlayingPerl::new();
         MediaControl { npp }
     }
     pub fn get_media(&self) -> Option<Media> {
-        let npp = self.npp.as_ref()?;
-        let guard = npp.get_info();
+        let guard = self.npp.get_info();
         let info = guard.as_ref()?;
         Some(Media {
             title: info.title.clone()?,
